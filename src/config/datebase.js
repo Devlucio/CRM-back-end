3aa98e99a8893b6ca523ff6b39648ea92825812c
Lúcio Cecílio de Souza Júnior <devluciocdesouza@gmail.com>
2022-11-07 08:18:36 -0300

    Criando database

--- src/config/datebase.js
+++ src/config/datebase.js
@@ -1,10 +1,29 @@
 //Importar modulo ou pacote
-const express = require("express");
+//*const express = require("express");
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI;
 
 //instaciar pacote (deixar disponível)
-const datebase = express();
+//*const datebase = express();
 
-const message = console.log("Olá, estou aqui (datebase)")
+const connect = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log("Banco conectado :)")
+    } catch (error) {
+        console.error("Erro: ", error.message)
+    }
+}
+   
+    
+
+//*const message = console.log("Olá, estou aqui (datebase)")
 
 //exportar modulo
-module.exports= datebase
+module.exports= {
+    connect
+}
